a066e0f31e69036af257b8e4b93aaabfe88cc159
Marcin Radoszewski <marcin.radoszewski@allegro.pl>
2021-12-01 07:18:44 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var count = 0
+        var last = input.first()
+        input.drop(1).forEach {
+            if (last < it) {
+                count++
+            }
+            last = it
+        }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        fun window(index: Int): Int {
+            return input[index] + input[index+1] + input[index+2]
+        }
+
+        val windows = (0..input.size-3).map { window(it) }
+
+        return part1(windows)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
