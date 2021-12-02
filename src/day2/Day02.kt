c908943fc8b683a6eae7e8acde3e889e63e63168
Marcin Radoszewski <marcin.radoszewski@allegro.pl>
2021-12-02 06:33:40 +0100

    day 2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -3,19 +3,52 @@ package day2
 import readInput
 
 fun main() {
-    fun part1(input: List<Int>): Int {
-        return 0
+    fun part1(input: List<String>): Int {
+        val cmds = input.parseCommands()
+        val forward = cmds.filterCommands("forward").mapToValue().sum()
+        val up = cmds.filterCommands("up").mapToValue().sum()
+        val down = cmds.filterCommands("down").mapToValue().sum()
+
+        val horizontal = forward
+        val depth = down - up
+
+        return horizontal * depth
     }
 
-    fun part2(input: List<Int>): Int {
-        return 0
+    fun part2(input: List<String>): Int {
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+        input.parseCommands().forEach {
+            when(it.dir) {
+                "forward" -> {
+                    horizontal += it.value
+                    depth += it.value * aim
+                }
+                "up" -> aim -= it.value
+                "down" -> aim += it.value
+            }
+        }
+        return horizontal * depth
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day2/input_test").map { it.toInt() }
-    check(part1(testInput) == 0)
+    val testInput = readInput("day2/input_test")
+    check(part2(testInput) == 900)
 
-    val input = readInput("day2/input").map { it.toInt() }
+    val input = readInput("day2/input")
     println(part1(input))
     println(part2(input))
 }
+
+data class Command(val dir: String, val value: Int)
+
+fun List<String>.parseCommands() = map {
+    val (dir, value) = it.split(" ")
+    Command(dir, value.toInt())
+}
+
+fun List<Command>.filterCommands(vararg commands: String) =
+    filter { commands.contains(it.dir) }
+
+fun List<Command>.mapToValue() = map { it.value }
