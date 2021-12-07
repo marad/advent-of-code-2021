60bd53ea8f8e1b9e4fd83e1096d0428adfdff0bf
Marcin Radoszewski <moriturius@gmail.com>
2021-12-07 06:23:59 +0100

    day 7 and 8

--- src/day7/Code.kt
+++ src/day7/Code.kt
@@ -1,21 +1,34 @@
 package day7
 
 import readInput
+import kotlin.math.absoluteValue
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun solve(input: List<Int>, calcFuel: (Int, List<Int>) -> Int): Int {
+        val max = input.maxOf { it }
+        return (0 until max).map { calcFuel(it, input) }
+            .minOf { it }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day7/input_test")
-    check(part2(testInput) == 0)
+    val testInput = readInput("day7/input_test").first().split(",").map { it.toInt() }
+    check(solve(testInput, ::linear) == 37)
 
-    val input = readInput("day7/input")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("day7/input").first().split(",").map { it.toInt() }
+    println("Part 1 ${solve(input, ::linear)}")
+
+    check(solve(testInput, ::summing) == 168)
+    println("Part 2 ${solve(input, ::summing)}")
+}
+
+fun linear(position: Int, input: List<Int>): Int {
+    return input.map { (it-position).absoluteValue }.sum()
+}
+
+fun summing(position: Int, input: List<Int>): Int {
+    return input.map {
+        val diff = (it-position).absoluteValue
+        (0..diff).sum()
+    }.sum()
 }
