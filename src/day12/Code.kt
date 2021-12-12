3ef8592c2803d2cd0b2ab9dcc42494c3ebb11dfc
Marcin Radoszewski <moriturius@gmail.com>
2021-12-12 08:24:28 +0100

    Refactor day12

--- src/day12/Code.kt
+++ src/day12/Code.kt
@@ -8,48 +8,13 @@ class Foo
 fun main() {
     fun part1(input: List<String>): Int {
         val graph = makeGraph(input)
-        val toVisit = ArrayDeque<List<String>>()
-        toVisit.addFirst(listOf("start"))
-        val paths = mutableListOf<List<String>>()
-        while(toVisit.isNotEmpty()) {
-            val currentPath = toVisit.removeFirst()
-            val visiting = currentPath.last()
-
-            if (currentPath.canVisit(visiting)) {
-                graph[visiting]!!.forEach {
-                    if (it == "end") {
-                        paths.add(currentPath + it)
-                    } else {
-                        toVisit.addLast(currentPath + it)
-                    }
-                }
-            }
-        }
-
+        val paths = findPathsMatchingCriteria(graph, ::isValidPathPart1)
         return paths.size
     }
 
     fun part2(input: List<String>): Int {
-
         val graph = makeGraph(input)
-        val toVisit = ArrayDeque<List<String>>()
-        toVisit.addFirst(listOf("start"))
-        val paths = mutableListOf<List<String>>()
-        while(toVisit.isNotEmpty()) {
-            val currentPath = toVisit.removeFirst()
-            val visiting = currentPath.last()
-
-            if (currentPath.isValidPath()) {
-                graph[visiting]!!.forEach {
-                    if (it == "end") {
-                        paths.add(currentPath + it)
-                    } else {
-                        toVisit.addLast(currentPath + it)
-                    }
-                }
-            }
-        }
-
+        val paths = findPathsMatchingCriteria(graph, ::isValidPathPart2)
         return paths.size
     }
 
@@ -64,11 +29,37 @@ fun main() {
     println("part 2 solution: ${part2(input)}")
 }
 
-fun List<String>.canVisit(v: String) =
-    v[0].isUpperCase() || !dropLast(1).contains(v)
+typealias Path = List<String>
+typealias Graph = Map<String, List<String>>
 
-fun List<String>.isValidPath(): Boolean {
-    val smallCaveVisitCounts = filter { it[0].isLowerCase() }.groupBy { it }.mapValues { it.value.count() }
+fun findPathsMatchingCriteria(graph: Graph, criteria: (Path) -> Boolean): List<Path> {
+    val toVisit = ArrayDeque<List<String>>()
+    toVisit.addFirst(listOf("start"))
+    val paths = mutableListOf<Path>()
+
+    while(toVisit.isNotEmpty()) {
+        val currentPath = toVisit.removeFirst()
+        val visiting = currentPath.last()
+
+        if (criteria(currentPath)) {
+            graph[visiting]!!.forEach {
+                if (it == "end") {
+                    paths.add(currentPath + it)
+                } else {
+                    toVisit.addLast(currentPath + it)
+                }
+            }
+        }
+    }
+    return paths
+}
+
+fun isValidPathPart1(it: Path): Boolean =
+    it.last()[0].isUpperCase() || !it.dropLast(1).contains(it.last())
+
+fun isValidPathPart2(path: Path): Boolean {
+    // Ah, the brute force here is overwhelming, but gets the job done :D
+    val smallCaveVisitCounts = path.filter { it[0].isLowerCase() }.groupBy { it }.mapValues { it.value.count() }
     if (smallCaveVisitCounts.getOrDefault("start", 0) > 1) return false
     if (smallCaveVisitCounts.getOrDefault("end", 0) > 1) return false
     val countOfSmallCavesVisitedMoreThanOnce = smallCaveVisitCounts.count { it.value > 1 }
@@ -76,7 +67,7 @@ fun List<String>.isValidPath(): Boolean {
     return countOfSmallCavesVisitedMoreThanOnce <= 1 && cavesVisitedAtMostTwice
 }
 
-fun makeGraph(input: List<String>): Map<String, List<String>> {
+fun makeGraph(input: List<String>): Graph {
     val map = mutableMapOf<String, MutableList<String>>()
     input.forEach {
         val (a, b) = it.split("-")
