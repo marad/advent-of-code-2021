830cfa0479ef9e1e78a49d14f5f976f07bca5729
Marcin Radoszewski <moriturius@gmail.com>
2021-12-16 14:03:31 +0100

    Solution to day 16

--- src/day16/Code.kt
+++ src/day16/Code.kt
@@ -2,10 +2,170 @@ package day16
 
 import io.kotest.matchers.shouldBe
 import readInput
+import java.lang.RuntimeException
 
 class Foo
 
-val charMapping = mapOf<Char, String>(
+fun main() {
+    fun part1(input: List<String>): Int {
+        val string = input.first().toCharArray().joinToString("") { charMapping[it]!! }
+        val parser = Parser(string)
+        val packet = parser.parsePacket()
+        return packet.sumVersions()
+    }
+
+    fun part2(input: List<String>): Long {
+        val string = input.first().toCharArray().joinToString("") { charMapping[it]!! }
+        val parser = Parser(string)
+        val packet = parser.parsePacket()
+        println(packet.print())
+        println(parser.tail())
+        return packet.calc()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("${Foo::class.java.packageName}/input_test")
+    part1(testInput) shouldBe 20
+
+    val input = readInput("${Foo::class.java.packageName}/input")
+    println("part 1 solution: ${part1(input)}")
+
+    part2(listOf("C200B40A82")) shouldBe 3
+    part2(listOf("04005AC33890")) shouldBe 54
+    part2(listOf("880086C3E88112")) shouldBe 7
+    part2(listOf("CE00C43D881120")) shouldBe 9
+    part2(listOf("D8005AC2A8F0")) shouldBe 1
+    part2(listOf("F600BC2D8F")) shouldBe 0
+    part2(listOf("9C005AC2F8F0")) shouldBe 0
+    part2(listOf("9C0141080250320F1802104A08")) shouldBe 1
+    println("part 2 solution: ${part2(input)}")
+}
+
+sealed interface Packet {
+    val version: Int
+    val type: Int
+    fun sumVersions(): Int
+    fun calc(): Long
+    fun print(): String
+}
+
+data class Operator(override val version: Int, override val type: Int, val subpackets: List<Packet>): Packet {
+    override fun sumVersions(): Int {
+        return version + subpackets.sumOf { it.sumVersions() }
+    }
+
+    override fun calc(): Long {
+        val subpacketValues = subpackets.map { it.calc() }
+        return when(type) {
+            TYPE_SUM -> subpacketValues.sum()
+            TYPE_MUL -> subpacketValues.reduce { a, b -> a*b }
+            TYPE_MIN -> subpacketValues.minOf { it }
+            TYPE_MAX -> subpacketValues.maxOf { it }
+            TYPE_GT -> if (subpacketValues[0] > subpacketValues[1]) 1 else 0
+            TYPE_LT -> if (subpacketValues[0] < subpacketValues[1]) 1 else 0
+            TYPE_EQ -> if (subpacketValues[0] == subpacketValues[1]) 1 else 0
+            else -> throw RuntimeException("Unknown operation $type")
+        }
+    }
+
+    override fun print(): String {
+        val subpacketValues = subpackets.joinToString(" ") { it.print() }
+        return when(type) {
+            TYPE_SUM -> "(+ $subpacketValues)"
+            TYPE_MUL -> "(* $subpacketValues)"
+            TYPE_MIN -> "(min $subpacketValues)"
+            TYPE_MAX -> "(max $subpacketValues)"
+            TYPE_GT -> "(gt $subpacketValues)"
+            TYPE_LT -> "(lt $subpacketValues)"
+            TYPE_EQ -> "(eq $subpacketValues)"
+            else -> throw RuntimeException("Unknown operation $type")
+        }
+    }
+}
+
+data class Literal(override val version: Int, val value: Long): Packet {
+    override val type: Int = 4
+    override fun sumVersions(): Int = version
+    override fun calc(): Long  = value
+    override fun print(): String = value.toString()
+}
+
+const val TYPE_SUM = 0
+const val TYPE_MUL = 1
+const val TYPE_MIN = 2
+const val TYPE_MAX = 3
+const val TYPE_LITERAL = 4
+const val TYPE_GT = 5
+const val TYPE_LT = 6
+const val TYPE_EQ = 7
+const val LENTYPE_BITCOUNT = 0
+const val LENTYPE_SUBPACKETCOUNT = 1
+
+class Parser(input: String) {
+    private var position: Int = 0
+    private var chars = input.toCharArray()
+
+    fun hasMore() = (chars.size - position) >= 11
+
+    fun parsePacket(): Packet {
+        val version = parseVersion()
+        val type = parseType()
+        return if (type == TYPE_LITERAL) {
+            parseLiteralPacket(version)
+        } else {
+            val lengthType = parseLengthTypeId()
+            if (lengthType == LENTYPE_BITCOUNT) {
+                val subpacketBits = getNextInt(15)
+                val subParser = Parser(getNextBits(subpacketBits))
+                val subpackets = mutableListOf<Packet>()
+                while(subParser.hasMore()) {
+                    subpackets.add(subParser.parsePacket())
+                }
+                return Operator(version, type, subpackets)
+            } else if (lengthType == LENTYPE_SUBPACKETCOUNT) {
+                val subpacketCount = getNextInt(11)
+                val subpackets = mutableListOf<Packet>()
+                repeat(subpacketCount) {
+                    subpackets.add(parsePacket())
+                }
+                return Operator(version, type, subpackets)
+            } else {
+                throw RuntimeException("How, on earth?!")
+            }
+        }
+    }
+
+    fun parseLiteralPacket(version: Int): Literal =
+        Literal(version, parseLiteralValue())
+
+    fun parseVersion(): Int = getNextInt(3)
+    fun parseType(): Int = getNextInt(3)
+    fun parseLiteralValue(): Long {
+        var value = 0L
+        while(true) {
+            val isLast = getNextChar() == '0'
+            value = (value shl 4) or getNextInt(4).toLong()
+            if (isLast) break
+        }
+        return value
+    }
+    fun parseLengthTypeId(): Int = getNextInt(1)
+
+    fun tail(): String =
+        chars.drop(position).joinToString("")
+
+    private fun getNextChar(): Char = chars[position++]
+
+    private fun getNextInt(count: Int): Int = getNextBits(count).toInt(2)
+
+    private fun getNextBits(count: Int): String {
+        return chars.copyOfRange(position, position+count)
+            .joinToString("")
+            .also { position += count }
+    }
+}
+
+val charMapping = mapOf(
     '0' to "0000",
     '1' to "0001",
     '2' to "0010",
@@ -24,183 +184,3 @@ val charMapping = mapOf<Char, String>(
     'F' to "1111",
 )
 
-fun main() {
-//    arrayOf(
-//        0b1000_0000_0000_0000,
-//        0b1000_0000_0000_0011,
-//        0b0100_0000_0000_0001
-//    ).toIntArray()
-//        .getBits(30, 4)
-//        .let { println("ASD: " + it.toString(2)) }
-
-    fun part1(input: List<String>): Int {
-        val packet = readPacket(input)
-//        val string = packet.joinToString("") { it.toString(2).padStart(12, '0') }
-        val string = input.first().toCharArray().joinToString("") { charMapping[it]!! }
-        println(string)
-        val parser2 = Parser(string)
-        println("Packet: ${parser2.parsePacket()}")
-        println("rest: ${parser2.tail()}")
-
-        return 0
-    }
-
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("${Foo::class.java.packageName}/input_test")
-    part1(testInput) shouldBe 31
-
-    val input = readInput("${Foo::class.java.packageName}/input")
-    println("part 1 solution: ${part1(input)}")
-
-    part2(testInput) shouldBe 0
-    println("part 2 solution: ${part2(input)}")
-}
-
-fun String.version(): Int = substring(0, 3).toInt(2)
-fun String.type(): Int = substring(3, 6).toInt(2)
-fun String.lengthTypeId(): Int = substring(6, 7).toInt(2)
-fun String.subpacketsLength(): Int = substring(7, 22).toInt(2)
-
-sealed interface Packet {
-    val version: Int
-    val type: Int
-}
-
-data class Operator(override val version: Int, override val type: Int, val subpackets: List<Packet>): Packet
-data class Literal(override val version: Int, val value: Int): Packet {
-    override val type: Int = 4
-}
-
-
-class Parser(input: String) {
-    private var position: Int = 0
-    private var chars = input.toCharArray()
-
-    fun parsePacket(): Packet {
-        val version = parseVersion()
-        val type = parseType()
-        return if (type == 4) {
-            parseLiteralPacket(version)
-        } else {
-            TODO()
-        }
-    }
-
-    fun parseLiteralPacket(version: Int): Literal =
-        Literal(version, parseLiteralValue())
-
-    fun parseVersion(): Int = getNextInt(3)
-    fun parseType(): Int = getNextInt(3)
-    fun parseLiteralValue(): Int {
-        var value = 0
-        while(true) {
-            val isLast = getNextChar() == '0'
-            value = (value shl 4) or getNextInt(4)
-            if (isLast) break;
-        }
-        return value
-    }
-
-    fun pos(): Int = position
-    fun tail(): String =
-        chars.drop(position).joinToString("")
-
-    private fun getNextChar(): Char = chars[position++]
-
-    private fun getNextInt(count: Int): Int = getNextBits(count).toInt(2)
-
-    private fun getNextBits(count: Int): String {
-        return chars.copyOfRange(position, position+count)
-            .joinToString("")
-            .also { position += count }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fun version(packet: IntArray): Int {
-    return packet[0] shr 13
-}
-
-// type 4 is literal value
-// any other is operator packet
-fun type(packet: IntArray): Int {
-    return (packet[0] shr 10) and 0b111
-}
-
-// 0 means that next 15 bits represent the number of bits
-// in subpackets
-// 1 means that next 11 bits number of subpackets
-fun lengthTypeId(packet: IntArray): Int {
-    return (packet[0] shr 7) and 0b1
-}
-
-// 0011100000000000
-// VVVTTTILLLLLLLLL
-
-fun contentsLength(packet: IntArray): Int {
-    val head = packet[0] and 0b111111111
-    val tail = (packet[1] shr 10)
-    return (head shl 6) or tail
-}
-
-// 1101 0010 1111 1110    0010 1000
-fun literalValue(packet: IntArray): Int {
-
-    fun isLastGroup(nth: Int): Boolean {
-        val bitPos = 6 + nth * 5
-        return packet.getBits(bitPos, 1) == 0
-    }
-
-    fun getValueBits(nth: Int): Int {
-        val bitPos = 6 + nth * 5
-        println("Getting ${bitPos+1}...")
-        return packet.getBits(bitPos+1, 4)
-    }
-
-    var value = 0.toULong()
-    var index = 0
-    while(true) {
-        val bits = getValueBits(index)
-        println("part $index: ${bits.toString(2)}")
-        value = (value shl 4) or bits.toULong()
-        if (isLastGroup(index)) break
-        index += 1
-    }
-
-    return value.toInt()
-}
-
-fun IntArray.getBits(start: Int, length: Int): Int {
-    val word = start / 16
-    val startInWord = start % 16
-    val end = 32 - startInWord - length
-    val value = (get(word).toUInt() shl 16) or
-            (getOrElse(word+1) {0}).toUInt()
-    return (((value shl startInWord) shr startInWord) shr end).toInt()
-}
-
-fun readPacket(input: List<String>): IntArray {
-    val line = input.first()
-    val missingBytes = line.length % 4
-//    return line.chunked(4).map { it.toInt(16) }.toIntArray()
-    val padded = line.padEnd(line.length+missingBytes, '0')
-    return padded.chunked(4).map { it.toInt(16) }.toIntArray()
-}
