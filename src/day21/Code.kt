1482822c2e2364b81a3573b6163fdba8e6ce6f70
Marcin Radoszewski <moriturius@gmail.com>
2021-12-21 13:22:37 +0100

    Functional refactor

--- src/day21/Code.kt
+++ src/day21/Code.kt
@@ -22,14 +22,16 @@ class Foo
 fun main() {
     fun part1(input: List<String>): Int {
         val die = DeterministicDie()
-        val game = Game(
+        val gameState = GameState(
             startingPosition(input[0]),
             startingPosition(input[1]),
-            winningScore = 1000,
-            rollPerTurnCount = 3,
         )
-        game.simulate(die)
-        return (game.loosingPlayerScore() * die.rollCount()).toInt()
+        val settings = GameSettings(winningScore = 1000, rollPerTurnCount = 3)
+        simulate(gameState,
+            updateState = { updateState(it, settings, die) },
+            isDone = { isDone(it, settings) })
+
+        return (gameState.loosingPlayerScore() * die.rollCount()).toInt()
     }
 
     fun part2(input: List<String>): Long {
@@ -37,8 +39,9 @@ fun main() {
         val pos2 = startingPosition(input[1])
 
         val startTime = System.currentTimeMillis()
-        val game = Game(pos1, pos2, winningScore = 21, rollPerTurnCount = 1)
-        val winningUniverses = countWinningProbabilities(game)
+        val gameState = GameState(pos1, pos2)
+        val calculator = UniversesCalculator(GameSettings(winningScore = 21, rollPerTurnCount = 1))
+        val winningUniverses = calculator.calculateWinningUniverses(gameState)
 
         println(winningUniverses)
 
@@ -64,29 +67,30 @@ fun main() {
     println("part 2 solution: ${part2(input)}")
 }
 
-fun countWinningProbabilities(game: Game): WinningUniverses {
-    return runBlocking(Dispatchers.Default) {
-        (3..9).pmap { calcResults(game.clone(), it, 1) }.sum()
-    }
-}
-
-fun calcResults(game: Game, currentRoll: Int, universes: Long): WinningUniverses {
-    game.tick(ConstantDie(currentRoll))
-    val thisRollUniverses = universes * rollUniverses(currentRoll)
-    return if (game.isDone()) {
-        when(game.winningPlayer()) {
-            1 -> WinningUniverses(thisRollUniverses, 0L)
-            2 -> WinningUniverses(0L, thisRollUniverses)
-            else -> throw RuntimeException("There are no other players!")
+class UniversesCalculator(private val gameSettings: GameSettings) {
+    fun calculateWinningUniverses(initialGameState: GameState): WinningUniverses {
+        return runBlocking(Dispatchers.Default) {
+            (3..9).pmap { calcResults(initialGameState.clone(), it, 1) }.sum()
         }
-    } else {
-        (3..9).map {
-            calcResults(game.clone(), it, thisRollUniverses)
-        }.sum()
+    }
+
+    private fun calcResults(gameState: GameState, currentRoll: Int, universes: Long): WinningUniverses {
+        updateState(gameState, gameSettings, ConstantDie(currentRoll))
+        val thisRollUniverses = universes * rollUniverses(currentRoll)
+        return if (isDone(gameState, gameSettings)) {
+            when(gameState.winningPlayer()) {
+                1 -> WinningUniverses(thisRollUniverses, 0L)
+                2 -> WinningUniverses(0L, thisRollUniverses)
+                else -> throw RuntimeException("There are no other players!")
+            }
+        } else {
+            (3..9).map {
+                calcResults(gameState.clone(), it, thisRollUniverses)
+            }.sum()
+        }
     }
 }
 
-fun startingPosition(line: String): Int = line.substring(28).toInt()
 
 data class WinningUniverses(
     val player1: Long,
@@ -102,57 +106,69 @@ data class WinningUniverses(
 
 fun List<WinningUniverses>.sum() = reduce { a, b -> a + b }
 
-class Game(var player1Position: Int,
-           var player2Position: Int,
-           val winningScore: Int,
-           val rollPerTurnCount: Int
+fun simulate(state: GameState, updateState: (GameState) -> Unit, isDone: (GameState) -> Boolean) {
+    while(!isDone(state)) {
+        updateState(state)
+    }
+}
+
+fun isDone(state: GameState, settings: GameSettings) =
+    state.player1Score >= settings.winningScore || state.player2Score >= settings.winningScore
+
+fun updateState(state: GameState, settings: GameSettings, die: Die) {
+    var roll = 0
+    repeat(settings.rollPerTurnCount) {
+        roll += die.roll()
+    }
+
+    if (state.player1Turn) {
+        state.player1Position += roll
+        while(state.player1Position > 10)
+            state.player1Position -= 10
+        state.player1Score += state.player1Position
+    } else {
+        state.player2Position += roll
+        while(state.player2Position > 10)
+            state.player2Position -= 10
+        state.player2Score += state.player2Position
+    }
+    state.player1Turn = !state.player1Turn
+    state.turns += 1
+}
+
+data class GameSettings(val winningScore: Int, val rollPerTurnCount: Int)
+
+class GameState(var player1Position: Int,
+                var player2Position: Int,
 ) {
     var turns: Int = 0
     var player1Turn = true
     var player1Score: Long = 0
     var player2Score: Long = 0
 
-    fun clone(): Game {
-        val game = Game(player1Position, player2Position, winningScore, rollPerTurnCount)
-        game.turns = turns
-        game.player1Turn = player1Turn
-        game.player1Score = player1Score
-        game.player2Score = player2Score
-        return game
+    fun clone(): GameState {
+        val gameState = GameState(player1Position, player2Position)
+        gameState.turns = turns
+        gameState.player1Turn = player1Turn
+        gameState.player1Score = player1Score
+        gameState.player2Score = player2Score
+        return gameState
     }
 
     fun winningPlayer(): Int = if (player1Score > player2Score) 1 else 2
-
     fun loosingPlayerScore() = Math.min(player1Score, player2Score)
+}
 
-    fun isDone(): Boolean = player1Score >= winningScore || player2Score >= winningScore
 
-    fun simulate(die: Die) {
-        while(!isDone()) {
-            tick(die)
-        }
-    }
-
-    fun tick(die: Die) {
-        var roll = 0
-        repeat(rollPerTurnCount) {
-            roll += die.roll()
-        }
-
-        if (player1Turn) {
-            player1Position += roll
-            while(player1Position > 10)
-                player1Position -= 10
-            player1Score += player1Position
-        } else {
-            player2Position += roll
-            while(player2Position > 10)
-                player2Position -= 10
-            player2Score += player2Position
-        }
-        player1Turn = !player1Turn
-        turns += 1
-    }
+fun rollUniverses(roll: Int): Long = when(roll) {
+    3 -> 1L
+    4 -> 3L
+    5 -> 6L
+    6 -> 7L
+    7 -> 6L
+    8 -> 3L
+    9 -> 1L
+    else -> throw RuntimeException("Roll $roll should not have happend.")
 }
 
 interface Die {
@@ -180,16 +196,7 @@ class ConstantDie(val value: Int) : Die {
     override fun roll(): Int = value
 }
 
-fun rollUniverses(roll: Int): Long = when(roll) {
-    3 -> 1L
-    4 -> 3L
-    5 -> 6L
-    6 -> 7L
-    7 -> 6L
-    8 -> 3L
-    9 -> 1L
-    else -> throw RuntimeException("Roll $roll should not have happend.")
-}
+fun startingPosition(line: String): Int = line.substring(28).toInt()
 
 suspend fun <A, B> Iterable<A>.pmap(f: suspend (A) -> B): List<B> = coroutineScope {
     map { async { f(it) } }.awaitAll()
