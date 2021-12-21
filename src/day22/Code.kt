ce90327662afbcd5a78b6149f7ca252f70d3bbf7
Marcin Radoszewski <moriturius@gmail.com>
2021-12-21 11:47:46 +0100

    Prepare day 22

---
+++ src/day22/Code.kt
@@ -0,0 +1,28 @@
+package day22
+
+import io.kotest.matchers.shouldBe
+import readInput
+
+class Foo
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("${Foo::class.java.packageName}/input_test")
+    part1(testInput) shouldBe 0
+    println("part 1 test ok")
+
+    val input = readInput("${Foo::class.java.packageName}/input")
+    println("part 1 solution: ${part1(input)}")
+
+    part2(testInput) shouldBe 0
+    println("part 2 test ok")
+    println("part 2 solution: ${part2(input)}")
+}
